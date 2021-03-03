1d55fedb7e9dc105323016376d2f52e733422807
Jørgen Vatle <Jorgen@WcAServices.net>
2021-03-03 15:43:30 +0100

    Add CLI interface.

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import Minimist from 'minimist';
+import Inquirer, { Question, QuestionCollection } from 'inquirer';
+import Parser from './Parser';
+
+type InputSource = 'clipboard' | 'file' | 'pipe';
+
+interface CliOptions {
+    immediate: boolean;
+    source: InputSource;
+}
+
+const availableSources: InputSource[] = ['clipboard'];
+const options = Minimist<CliOptions>(process.argv.splice(2), {
+    boolean: ['immediate'],
+    string: ['source'],
+    alias: {
+        i: 'immediate',
+        s: 'source',
+    }
+});
+
+(async () => {
+    const source = options.source || 'clipboard';
+
+    if (!availableSources.includes(source)) {
+        console.error(`'%s' is not an allowed input source. Please enter one of the following as the '-s' argument: %s`, options.source, availableSources);
+    }
+
+    const parser = new Parser();
+    await parser.validateTextContent();
+
+    if (options.immediate) {
+        await parser.save();
+        return;
+    }
+
+    parser.print();
+
+    const prompt = await Inquirer.prompt({
+        name: 'shouldCopy',
+        type: 'confirm',
+        message: `Detected ${parser.result.length} entries. Copy the above JSON to your clipboard?`
+    });
+
+    if (prompt.shouldCopy) {
+        await parser.save();
+    }
+})();
