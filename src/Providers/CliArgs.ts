dc3422e054b755cf88fd72edea7dc8ca09bd13b1
Jørgen Vatle <Jorgen@WcAServices.net>
2022-04-10 16:41:47 +0200

    Update helper style for jippy.

--- src/Providers/CliArgs.ts
+++ src/Providers/CliArgs.ts
@@ -1,4 +1,5 @@
 import { parse as Parser } from 'ts-command-line-args';
+import Chalk from 'chalk';
 
 const AvailableInputSources = ['clipboard'];
 const Package = require('../../package.json');
@@ -7,15 +8,20 @@ export default Parser<CliOptions>({
     source: {
         type: (value: InputSource) => {
             if (!AvailableInputSources.includes(value)) {
-                console.error(`'%s' is not an allowed input source. Please enter one of the following as the '-s' argument: %s`, value, AvailableInputSources);
+                console.error(
+                    `\n${Chalk.bgRed(`Oops, ${Chalk.underline(value)} not a supported input source!`)}\n\nTry the following instead:\n%s\n\nOr for more help:\n%s`,
+                    `$\t jippy --source ${Chalk.yellowBright(AvailableInputSources[0])}`,
+                    `$\t jippy --help`,
+                );
                 process.exit(22);
             }
+
             return value;
         },
         alias: 's',
         defaultValue: 'clipboard',
         description: `Whether we should grab text from your clipboard or a file`,
-        typeLabel: AvailableInputSources.join(', ')
+        typeLabel: Chalk.yellowBright(AvailableInputSources.join(', '))
     },
     immediate: {
         type: Boolean,
