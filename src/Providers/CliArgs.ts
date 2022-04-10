4c54a33debb59e0d1a832985045763cb11d9de78
Jørgen Vatle <Jorgen@WcAServices.net>
2022-04-10 17:05:51 +0200

    Add obfuscate option to CliArgs.

--- src/Providers/CliArgs.ts
+++ src/Providers/CliArgs.ts
@@ -2,6 +2,7 @@ import { parse as Parser } from 'ts-command-line-args';
 import Chalk from 'chalk';
 
 const AvailableInputSources = ['clipboard'];
+const ObfuscateOptions = ['emails']
 const Package = require('../../package.json');
 
 export default Parser<CliOptions>({
@@ -29,6 +30,22 @@ export default Parser<CliOptions>({
         defaultValue: false,
         description: 'Skip the confirmation prompt - just push directly to the clipboard and hope for the best! 🙏'
     },
+    obfuscate: {
+        type: (value: Obfuscate) => {
+            if (!ObfuscateOptions.includes(value)) {
+                console.error(
+                    `\n${Chalk.bgRed(`Oops, ${Chalk.underline(value)} not a supported obfuscate option!`)}\n\nTry the following instead:\n%s\n\nOr for more help:\n%s`,
+                    `$\t jippy --obfuscate ${Chalk.yellowBright(ObfuscateOptions[0])}`,
+                    `$\t jippy --help`,
+                );
+                process.exit(22);
+            }
+            return value;
+        },
+        alias: 'o',
+        multiple: true,
+        description: 'Specify whether we should obfuscate the input text',
+    },
     help: {
         type: Boolean,
         description: 'Display this prompt - listing all available options.'
@@ -43,10 +60,12 @@ export default Parser<CliOptions>({
     ]
 });
 
-type InputSource = typeof AvailableInputSources[number];
+export type InputSource = typeof AvailableInputSources[number];
+export type Obfuscate = typeof ObfuscateOptions[number];
 interface CliOptions {
     immediate: boolean;
     source: InputSource;
+    obfuscate: Obfuscate[],
     help: boolean;
 }
 
